67bbba29f783b5a017e2c97f0a45d9aca6fba22f
Andrei Amza <andrei.amza177@gmail.com>
2025-03-27 14:24:26 +0000

    fix: proxy.ts named export + clear stale build

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,11 @@
 import NextAuth from 'next-auth'
 import { authConfig } from './auth.config'
 
-export const { auth: proxy } = NextAuth(authConfig)
+const { auth } = NextAuth(authConfig)
+
+export async function proxy(request: Parameters<typeof auth>[0]) {
+  return auth(request as Parameters<typeof auth>[0])
+}
 
 export const config = {
   matcher: ['/((?!_next/static|_next/image|favicon.ico|public).*)'],
